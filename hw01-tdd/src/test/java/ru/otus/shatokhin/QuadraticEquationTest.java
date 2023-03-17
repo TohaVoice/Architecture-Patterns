4318eaaff89196b8df7a812e8bfcae92daad643a
TohaVoice <TohaVoice@gmail.com>
2023-03-17 22:22:45 +0400

    Implemented the test of point 3 - test check expression out where hasn't roots

--- hw01-tdd/src/test/java/ru/otus/shatokhin/QuadraticEquationTest.java
+++ hw01-tdd/src/test/java/ru/otus/shatokhin/QuadraticEquationTest.java
@@ -1,11 +1,28 @@
 package ru.otus.shatokhin;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class QuadraticEquationTest {
 
-    @Test
-    void justTest() {
+    private static final double PRECISION = 1e-6;
 
+    @Test
+    @DisplayName("Check out x^2+1 = 0 doesn't have roots")
+    void caseHasNotRootsTest() {
+        double a = 1d;
+        double b = 0d;
+        double c = 1d;
+        double[] roots;
+
+        double d = b * b - 4 * a * c;
+        if (d < - PRECISION)
+            roots = new double[0];
+        else
+            roots = new double[1];
+
+        assertEquals(0, roots.length);
     }
 }
