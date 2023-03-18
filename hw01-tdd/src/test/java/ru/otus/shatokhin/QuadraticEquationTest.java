c45c93b0af161a51abef6fa0ba1917a3ff8a7091
TohaVoice <TohaVoice@gmail.com>
2023-03-18 09:53:11 +0400

    Implemented the point 5 - test check x^2-1 = 0 out there are two multiplicity roots 1 (x1=1, x2=-1)

--- hw01-tdd/src/test/java/ru/otus/shatokhin/QuadraticEquationTest.java
+++ hw01-tdd/src/test/java/ru/otus/shatokhin/QuadraticEquationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 public class QuadraticEquationTest {
 
@@ -18,7 +19,7 @@ public class QuadraticEquationTest {
     }
 
     @Test
-    @DisplayName("Check out x^2+1 = 0 doesn't have roots")
+    @DisplayName("Check out x^2+1 = 0 has no roots")
     void caseHasNotRootsTest() {
         double a = 1d;
         double b = 0d;
@@ -27,5 +28,23 @@ public class QuadraticEquationTest {
         double[] roots = quadraticEquation.solve(a, b, c);
 
         assertEquals(0, roots.length);
+    }
+
+    @Test
+    @DisplayName("Check out x^2-1 = 0 there are two multiplicity roots 1 (x1=1, x2=-1)")
+    void caseHasTwoRootsTest() {
+        double a = 1d;
+        double b = 0d;
+        double c = -1d;
+
+        double d = b * b - 4 * a * c;
+        double sqrt = Math.sqrt(d);
+        double x1 = (-b + sqrt) / (2 * a);
+        double x2 = (-b - sqrt) / (2 * a);
+        double[] roots = new double[]{x1, x2};
+
+        assertEquals(2, roots.length);
+        assertThat(roots[0]).isEqualTo(1, withPrecision(PRECISION));
+        assertThat(roots[1]).isEqualTo(-1, withPrecision(PRECISION));
     }
 }
