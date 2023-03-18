e8755eb6538670b7b93b2ba3e3b43347635a8d07
TohaVoice <TohaVoice@gmail.com>
2023-03-18 11:11:38 +0400

    Implemented the point 12 - refactoring the solve method to according previously point

--- hw01-tdd/src/main/java/ru/otus/shatokhin/QuadraticEquation.java
+++ hw01-tdd/src/main/java/ru/otus/shatokhin/QuadraticEquation.java
@@ -13,9 +13,12 @@ public class QuadraticEquation {
         }
 
         double d = b * b - 4 * a * c;
-        if (d < -precision)
+        if (Math.abs(d) < precision) {
+            double x = -b / (2 * a);
+            return new double[]{x, x};
+        } else if (d < -precision) {
             return new double[0];
-        else {
+        } else {
             double sqrt = Math.sqrt(d);
             double x1 = (-b + sqrt) / (2 * a);
             double x2 = (-b - sqrt) / (2 * a);
